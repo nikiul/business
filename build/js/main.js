23308ea6b667429ceb08a2c96075f617b51d9ce0
nikiul <nikulans@gmail.com>
2020-12-23 18:53:23 +0300

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,9 @@
+$(".banner").vegas({
+    cover: true,
+    timer: false,
+    slides: [
+        { src: "img/18.jpg" },
+        { src: "img/1.jpg" } 
+    ]
+});
+//header
---
+++ src/blocks/banner/banner.js
@@ -0,0 +1,8 @@
+$(".banner").vegas({
+    cover: true,
+    timer: false,
+    slides: [
+        { src: "img/18.jpg" },
+        { src: "img/1.jpg" } 
+    ]
+});
